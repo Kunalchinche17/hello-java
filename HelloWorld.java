5a9fbe6cba83a91f2e7348cb2f754dbf34027a82
Kunalchinche17 <kunalchinche1997@gmail.come>
2024-09-22 17:55:52 +0000

    add some lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
   public static void main(String[] args ) {
    System.out.println("Hello, World");
    System.out.println("Hare rama harea rama rama ram harea hare");
+   System.out.println("jay jay ram krushna hari");
+   System.out.println("jay jay ram krushna hari");
   }
 }
 
