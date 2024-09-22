828459027f343b76f50398c742e03d41df06fcb2
Kunalchinche17 <kunalchinche1997@gmail.come>
2024-09-22 09:35:46 +0000

    added java file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWold {
+  public static void main(string[] args ) {
+    System.out.println("Hello, World");
+    System.out.println("Hello, kunal");
+    System.out.println("kunal:Hi, madhu");
+  }
+}
