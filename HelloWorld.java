2d47ec33814a859c54062c9b305275bfe45f2a89
Kunalchinche17 <kunalchinche1997@gmail.come>
2024-09-22 17:26:56 +0000

    new changes delating some lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args ) {
-    System.out.println("Hello, World");
-    System.out.println("Hello, kunal");
-    System.out.println("kunal:Hi, madhu");
-    System.out.println("Hello, shyam");
-    System.out.println("Hello, ram");
-    System.out.println("Hello, okay bye....!!!!!");
+   System.out.println("Hello, World");
    System.out.println("Hare rama harea rama rama ram harea hare");
   }
 }
