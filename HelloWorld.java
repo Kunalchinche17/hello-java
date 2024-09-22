cc66c68345fba3fcdab7fcd97375df9c56fe7777
Kunalchinche17 <kunalchinche1997@gmail.come>
2024-09-22 15:00:21 +0000

    Added Bug-corrected file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
-public class HelloWold {
-  public static void main(string[] args ) {
+public class HelloWorld {
+  public static void main(String[] args ) {
     System.out.println("Hello, World");
     System.out.println("Hello, kunal");
     System.out.println("kunal:Hi, madhu");
   }
 }
+
+
+
+
+
+
